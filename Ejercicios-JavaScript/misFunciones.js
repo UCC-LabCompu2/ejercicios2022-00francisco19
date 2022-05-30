5e2690161569f85278948bc69fbbbf73cc36b127
00francisco19 <fransanchez.p19@gmail.com>
2022-05-30 18:56:01 -0300

    ejercicio grados radianes

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -30,3 +30,17 @@ function cambiarUnidades(id, valor){
         document.lasUnidades.unid_pie.value = 3 * valor;
         document.lasUnidades.unid_metro.value = 0.9144 * valor;}
 }
+
+function convertirGR(id){
+    var grad, rad;
+    if (id=="grados"){
+        grad = document.getElementById("grados").value;
+        rad = (grad*Math.PI)/180;
+    } else if (id=="radianes"){
+        rad = document.getElementById("radianes").value;
+        grad = (rad*180)/Math.PI
+    }
+    document.getElementById("grados").value = grad;
+    document.getElementById("radianes").value = rad;
+
+}
